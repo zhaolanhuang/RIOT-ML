416fc2b49afcf71ce0619272b019d7553a358e0e
Zhaolan Huang <zhaolan94@gmail.com>
2024-06-12 14:55:36 +0200

    fix: filter out parameters that merged/optimized-out via tvm

--- mlmci_utils.py
+++ mlmci_utils.py
@@ -3,9 +3,15 @@ from utils import extract_io_vars_from_module, generate_model_params_files, gene
 def generate_mlmci_files(relay_module, mod_params):
     module = relay_module
     params = mod_params
-    input_vars, output_vars = extract_io_vars_from_module(module)
-    input_vars = filter(lambda x: str(x['name']) not in params, input_vars)
+    tvm_input_vars, tvm_output_vars = extract_io_vars_from_module(module)
+    
+    # opt_params: some parameters will be merged into code, needs to be filtered out
+    metadata = module.executor_codegen_metadata
+    input_names = [str(i) for i in metadata.inputs]
+    opt_params = {k:v for k, v in params.items() if k in input_names}
+
+    input_vars = filter(lambda x: str(x['name']) not in params, tvm_input_vars)
     input_vars = list(input_vars)
-    generate_model_io_vars_files(input_vars, output_vars)
-    generate_model_params_files(params)
-    generate_model_binding_files(params, input_vars, output_vars)
+    generate_model_io_vars_files(input_vars, tvm_output_vars)
+    generate_model_params_files(opt_params)
+    generate_model_binding_files(opt_params, input_vars, tvm_output_vars)
